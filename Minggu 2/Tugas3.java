cfa7d2f6ecd65e43964b07875f852c6f3982c52e
WianAdiyatma <80669988+WianAdiyatma@users.noreply.github.com>
2021-03-16 11:18:21 +0700

    Add files via upload

---
+++ Minggu 2/Tugas3.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+public class Tugas3 {
+    
+    public static void main(String[] args){
+        
+        //Penulisan variable yang benar
+        boolean gameOver = false;
+        int student = 50, classes = 3;
+        double sales_tax;
+        short number1;
+
+        //Penulisan variable yang salah
+        int be = halo;
+        float myFloatNum = 9999.1111f;
+        double s = 019201909.9;
+        long class;
+    }
+}
+    
+
