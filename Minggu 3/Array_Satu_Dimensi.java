ac89a455c4a088d1d798b654bea3728723469d0b
WianAdiyatma <80669988+WianAdiyatma@users.noreply.github.com>
2021-03-24 10:42:28 +0700

    Add files via upload

---
+++ Minggu 3/Array_Satu_Dimensi.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package array_satu_dimensi;
+
+/**
+ *
+ * @author User
+ */
+public class Array_Satu_Dimensi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String [] nama = {"Wian", "Adiyatma", "Bisma", "Razaak"};
+        
+        for(int i=0; i<nama.length; i++) {
+            System.out.println(nama[i]);
+        }
+  
+    }
+    
+}
